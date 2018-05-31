e6da0ffd38833316e331b2ddb4f822f138502b7a
AnhDungHedspi61 <anhdung2941998@gmail.com>
2018-05-31 16:22:30 +0700

    add new file test

---
+++ danhsach.php
@@ -0,0 +1,18 @@
+ <p style="text-align:center;color:red;background:#0CF;padding:10px;">Món ăn nước</p>
+            <div class="danhsachsanpham">
+                <ul>
+                    <li><a href="#"> Mỳ ramen</a></li>
+                    <li><a href="#"> Ishikari nabe </a></li>
+                    <li><a href="#"> Yudofu</a></li>
+                    <li><a href="#"> Sanuki udon</a></li>
+                </ul>
+            </div>
+<p style="text-align:center;color:red;background:#0CF;padding:10px;">Món ăn khô</p>
+            <div class="danhsachsanpham">
+            	<ul>
+                	<li><a href="#"> Tempura </a></li>
+                    <li><a href="#"> Onigiri</a></li>
+                    <li><a href="#"> Natto</a></li>
+                    <li><a href="#"> Taiyaki</a></li>
+                </ul>
+            </div>
---
+++ footer.php
@@ -0,0 +1,3 @@
+ <div class="footer">
+    	<h3 style="color:#909;text-align:center;line-height:80px;">Studen of HUST</h3>
+    </div>
